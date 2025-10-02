52dfd23ebb4c79e35cbe73a85c6bf6c3895c3cae
Christoferis <dale.breuer@gmx.de>
2025-10-02 18:40:56 +0200

    Restructuring: Cannot reference Files as Webserver cant serve them (in the usual way)

---
+++ web/index/index.js
@@ -0,0 +1,39 @@
+// this script should do the following:
+// it updates the page every few seconds
+// it sends a sync task to host when pressing on sync button
+// it sends a sync task and redirects to timeset.html when pressing the set time button
+
+//TODO: synchronize on connect
+
+function sync(change)
+{
+    var date = new Date();
+    var ms = (date.getTime() - dat.getTimezoneOffset() * 60000) % 86400000;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://192.168.4.1/timeset', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send('sync=' + ms.toString());
+
+    
+    if (!change && xhr.status == 200) {
+        
+        window.alert("Time synchronized successfully");
+    } else if (!change) {
+        window.alert("Error synchronizing time");
+    }
+}
+
+function timechange()
+{
+    sync(true);
+    window.location.replace("192.168.4.1/timeedit");
+}
+
+// pollux labs
+function refresh(refreshPeriod) 
+{
+    setTimeout('location.reload(true)', refreshPeriod);
+}
+
+window.onload = refresh(5000);
---
+++ web/timeset/settings.js
@@ -0,0 +1,42 @@
+
+// return to dashboard without
+function toDashboard()
+{
+    window.location.replace("192.168.4.1/");
+}
+
+function setTime()
+{
+    // TODO:
+    // - get elements of HTML site
+    // - convert to millis
+    // - confirmation and redirect
+
+
+    // 86400000ms = 1day
+
+    //in ms
+    morning = document.getElementById("morninghours").value * 3600000 + document.getElementById("morningminutes").value * 60000
+    evening = document.getElementById("eveninghours").value * 3600000 + document.getElementById("eveningminutes").value * 60000
+
+    console.log(morning);
+
+    var date = new Date();
+    var ms = (date.getTime() - date.getTimezoneOffset() * 60000) % 86400000;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://192.168.4.1/timeset', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send('sync=' + ms.toString());
+    xhr.send('morning=' + morning.toString());
+    xhr.send('evening=' + evening.toString());
+
+    if (!change && xhr.status == 200) {
+        
+        window.alert("Time synchronized successfully");
+    } else if (!change) {
+        window.alert("Error synchronizing time");
+    }
+}
+
+
